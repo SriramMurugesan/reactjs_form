1eb15498a2180326c29f71f2f40d0f3a7d0f233d
SriramMurugesan <sri2014ram@gmail.com>
2025-08-11 18:50:49 +0530

    commit

--- react1/src/App.jsx
+++ react1/src/App.jsx
@@ -1,17 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Register from './Register';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-     <h1 className='text-3xl font-bold underline'>hello sri</h1>
-     
-    </>
-  )
+    <main className="App">
+      <Register />
+    </main>
+  );
 }
 
-export default App
+export default App;
